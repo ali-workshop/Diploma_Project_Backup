202a802f1c2d0aca7cf0351d04dedf023c5097f6
Noura-H-Mahmoud <nouramahmoud@Nouras-MacBook-Air.local>
2024-06-02 20:04:11 +0400

    for seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(AdminSeeder::class);
         $this->call(ContactSeeder::class);
-        $this->call(ContactSeeder::class);
         $this->call(MessageSeeder::class);
     }
 }
