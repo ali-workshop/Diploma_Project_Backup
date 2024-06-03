50e285e7d3deb5e3db9000d8dfa74527c087c7b7
TukaHeba <155295942+TukaHeba@users.noreply.github.com>
2024-06-03 05:18:13 +0300

    edit service index blade

--- resources/views/Admin/pages/dashboard/services/index.blade.php
+++ resources/views/Admin/pages/dashboard/services/index.blade.php
@@ -4,14 +4,15 @@
 <!-- Page Heading -->
 <h1 class="h3 mb-2 text-gray-800">Room Services</h1>
 
-<!-- DataTales -->
+<!-- DataTable -->
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-    <a href="{{ route('services.create') }}" class="btn btn-primary">New Service</a>
+        <a href="{{ route('services.create') }}" class="btn btn-primary">New Service</a>
     </div>
     <div class="card-body">
         <div class="table-responsive">
             <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <!-- Table Head -->
                 <thead>
                     <tr>
                         <th>#</th>
@@ -21,8 +22,10 @@
                         <th>Image</th>
                         <th></th>
                         <th></th>
+                        <th></th>
                     </tr>
                 </thead>
+                <!-- Table Body -->
                 <tbody>
                     @foreach($services as $service)
                     <tr>
@@ -30,7 +33,8 @@
                         <td>{{ $service->name}}</td>
                         <td>{{ $service->price}}</td>
                         <td>{{ $service->description}}</td>
-                        <td>{{ $service->img}}</td>
+                        <td><img src="{{ asset('images/' . $service->img) }}" alt="{{ $service->name }}" style="max-width: 100px;"></td>
+                        <td><a href="{{ route('services.show', $service->id) }}" class="btn btn-outline-dark">SHOW</a></td>
                         <td><a href='{{route("services.edit", $service->id)}}' class="btn btn-outline-success">EDIT</a></td>
                         <td>
                             <form action="{{ route('services.destroy', $service->id) }}" method="POST">
