2bde66dec7e9ab3146bad9d77fa7b1e0c5bb275a
TukaHeba <155295942+TukaHeba@users.noreply.github.com>
2024-06-04 02:01:34 +0300

    ServicesController API 3

--- app/Http/Controllers/Api/ServicesController.php
+++ app/Http/Controllers/Api/ServicesController.php
@@ -3,23 +3,33 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Service;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\ServiceResource;
 use App\Http\Traits\ApiResponserTrait;
+use App\Http\Resources\ServiceResource;
 
 class ServicesController extends Controller
 {
     use ApiResponserTrait;
     public function index()
     {
-        $services = Service::all();
-        return $this->successResponse(ServiceResource::collection($services)->toArray(request()), 'All available services', 200);
+        try {
+            $services = Service::all();
+            return $this->successResponse(ServiceResource::collection($services)->toArray(request()), 'All available services', 200);
+        } catch (\Exception $e) {
+            Log::error('Error in ServicesController@index: ' . $e->getMessage());
+            return $this->errorResponse('An error occurred: ' . $e->getMessage(), [], 500);
+        }
     }
     public function show(Service $service)
     {
-        if (!$service) {
-            return $this->errorResponse('Service does not exist', [], 404);
+        try {
+            if (!$service) {
+                return $this->errorResponse('Service does not exist', [], 404);
+            }
+            return $this->successResponse(['service' => new ServiceResource($service)], 'This is the service you requested', 200);        } catch (\Exception $e) {
+            Log::error('Error in ServicesController@show:' . $e->getMessage());
+            return $this->errorResponse('An error occurred: ' . $e->getMessage(), [], 500);
         }
-        return $this->successResponse(['service' => new ServiceResource($service)], 'This is the service you requested', 200);
     }
 }
