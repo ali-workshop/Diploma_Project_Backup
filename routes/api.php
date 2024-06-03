c8a81c005f7e6d70d7255cef3014bf73597959a7
TukaHeba <155295942+TukaHeba@users.noreply.github.com>
2024-06-03 05:26:18 +0300

    create controller+routes for api services

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\Api\ContactController;
-use App\Http\Controllers\Api\MessageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ContactController;
+use App\Http\Controllers\Api\MessageController;
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\ServicesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,4 +34,8 @@ Route::group(['prefix'=>'v1'],function(){
     });
 Route::post('/contacts',[ContactController::class,'store']);
 Route::post('/messages',[MessageController::class,'store']);
+
+// Services Open Routes
+Route::get('/services', [ServicesController::class, 'index']);
+Route::get('/services/{service}', [ServicesController::class, 'show']);
 
