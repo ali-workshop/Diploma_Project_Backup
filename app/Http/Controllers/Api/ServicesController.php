2d62d5ad1121441efa52febe21f3436d4838d752
TukaHeba <155295942+TukaHeba@users.noreply.github.com>
2024-06-04 00:57:13 +0300

    ServicesController API 1

--- app/Http/Controllers/Api/ServicesController.php
+++ app/Http/Controllers/Api/ServicesController.php
@@ -2,17 +2,24 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Service;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Resources\ServiceResource;
+use App\Http\Traits\ApiResponserTrait;
 
 class ServicesController extends Controller
 {
+    use ApiResponserTrait;
     public function index()
     {
-        //
+        $services = Service::all();
+        return $this->successResponse(ServiceResource::collection($services)->toArray(request()), 'All available categories', 200);
     }
-    public function show()
+    public function show(Service $service)
     {
-        //
+        if (!$service) {
+            return $this->errorResponse('Service does not exist', [], 404);
+        }
+        return $this->successResponse(['service' => new ServiceResource($service)], 'This is the service you requested', 200);
     }
 }
