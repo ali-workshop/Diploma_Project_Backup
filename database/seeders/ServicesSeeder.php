21976609f113dc94b8d5627b44d6e7facaab3c74
mona <mona-alrayes>
2024-06-06 21:43:59 +0300

    'update services prices in the seeder'

--- database/seeders/ServicesSeeder.php
+++ database/seeders/ServicesSeeder.php
@@ -15,61 +15,61 @@ class ServicesSeeder extends Seeder
     {
         Service::create([
             'name' => 'Single Bed',
-            'price'=> '50.00',
+            'price'=> '20.00',
             'description' => 'A simple single bed for one person.',
             'img' => 'services/SingleBed.jpg',
         ]);
         Service::create([
             'name' => 'Double Bed',
-            'price'=> '100.00',
+            'price'=> '40.00',
             'description' => 'A comfortable double bed for two person.',
             'img' => 'services/DoubleBed.jpg',
         ]);
         Service::create([
             'name' => 'King-size Bed',
-            'price'=> '200.00',
+            'price'=> '60.00',
             'description' => 'Luxery experience',
             'img' => 'services/KingSizeBed.jpg',
         ]);
         Service::create([
             'name' => 'TV',
-            'price'=> '30.00',
+            'price'=> '00.00',
             'description' => 'Full access to all channels',
             'img' => 'services/TV.jpg',
         ]);
         Service::create([
             'name' => 'Fitness Center',
-            'price'=> '100.00',
+            'price'=> '30.00',
             'description' => 'A Fitness Center offers modern equipment. Availabe 24/7',
             'img' => 'services/FitnessCenter.png',
         ]);
         Service::create([
             'name' => 'Outdoor Swimming Pool',
-            'price'=> '300.00',
+            'price'=> '30.00',
             'description' => 'An Outdoor Swimming Pool offers refreshing swims, sun lounging, and poolside service.',
             'img' => 'services/OutdoorSwimmingPool.jpg',
         ]);
         Service::create([
             'name' => 'Indoor Swimming Pool',
-            'price'=> '150.00',
+            'price'=> '20.00',
             'description' => 'An Indoor Swimming Pool offers relaxation and fun.',
             'img' => 'services/IndoorSwimmingPool.jpg',
         ]);
         Service::create([
             'name' => 'Breakfast',
-            'price'=> '150.00',
+            'price'=> '40.00',
             'description' => 'Our hotel offers a diverse breakfast buffet with local and international delicacies.',
             'img' => 'services/Breakfast.jpg',
         ]);
         Service::create([
             'name' => 'Lunch',
-            'price'=> '400.00',
+            'price'=> '70.00',
             'description' => 'Our hotel offers a variety of delicious lunch options in a relaxing atmosphere.',
             'img' => 'services/Lunch.jpg',
         ]);
         Service::create([
             'name' => 'Dinner',
-            'price'=> '500.00',
+            'price'=> '200.00',
             'description' => 'Our hotel offers an exquisite dinner menu in a cozy, elegant dining environment.',
             'img' => 'services/Dinner.png',
         ]);
