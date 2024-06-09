dcb2677e61ec7f709eb378a9c57b5a943c8b07e4
ALI ALI <alirafikali22@gmail.com>
2024-06-09 08:29:11 +0300

    fix bugs

--- app/Http/Requests/DateRangeRequest.php
+++ app/Http/Requests/DateRangeRequest.php
@@ -22,8 +22,8 @@ class DateRangeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'start_date' => 'required|date',
-            'end_date' => 'required|date|after:start_date',
+            'start_range' => 'required|date',
+            'end_range' => 'required|date|after:start_date',
         ];
     }
 }
